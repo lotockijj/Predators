241387a710c1135b607eac0393b7f4a233be1c02
winerr <winerr22@gmail.com>
2017-08-26 20:04:59 +0300

    Added load videos on page /Home/Video

--- BattleBands/BattleBands/Controllers/HomeController.cs
+++ BattleBands/BattleBands/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BattleBands.Data;
+using BattleBands.Models.VideoViewModels;
+using BattleBands.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BattleBands.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext _context;
+        UnitOfWork unitOfWork;
+        public HomeController(ApplicationDbContext context)
+        {
+            unitOfWork = new UnitOfWork(context);
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -11,10 +21,16 @@ namespace BattleBands.Controllers
         public IActionResult Performers() => View();
 
         public IActionResult Music() => View();
-
-        public IActionResult Video() => View();
-
-        public IActionResult Events() => View();
+        
+        public IActionResult Video()
+        {
+            var videos = unitOfWork.Videos.GetAll();
+            var items = new VideoView
+            {
+                Video = videos
+            };
+            return View(items);
+        }
 
         public IActionResult NewPerformer() => View();
 
---
+++ BattleBands/BattleBands/Models/VideoViewModels/VideoView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleBands.Models.VideoViewModels
+{
+    public class VideoView
+    {
+        public IEnumerable<ApplicationVideo> Video { get; set; }
+    }
+}
